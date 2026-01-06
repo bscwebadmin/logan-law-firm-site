32cab85ba283c8193a450019b2416d0dc9d03f14
Carlos Lerma <carlos@accoladesit.com>
2026-01-06 17:15:17 +0000

    Updated 2 files via CloudCannon.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true }
-}
-module.exports = nextConfig
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true
+};
+
+module.exports = nextConfig;
